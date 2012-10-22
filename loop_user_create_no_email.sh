7eb587a1b6e86d569e1e663c78ef6e4894e3d2bb
Paul <paulbosox@yahoo.com>
2012-10-22 14:07:42 -0400

    added clarification in the readme.  in the script changed to the correct file path for the input and outputing the results to a txt file

--- loop_user_create_no_email.sh
+++ loop_user_create_no_email.sh
@@ -6,7 +6,7 @@
 # in the example file just list their email addresses (fake) on line at a time
 
 # for user in $list
-for email in `cat /Users/paul/shell_scripts/drush_user_create/india/users_example_file_india`
+for email in `cat /Users/paul/shell_scripts/drush_user_create/atlantis/users_example_file_india`
    do
       	echo "Their email is $email"
       	sleep 1
@@ -27,6 +27,7 @@ echo " "
 echo "SQL Time"
 echo " "
 drush @live sqlq "UPDATE users SET DATA = (select data2 from dummytable) WHERE name = '$username'"
+echo $username >> user_list.txt
 
 echo " "
 echo " "
