00bbf06b46e96fa3cba714202d252fe1383ccab6
Paul <paulbosox@yahoo.com>
2012-11-03 18:51:11 -0400

    moved the file it generates with the username to the folder archive_users_created

--- loop_user_create_no_email.sh
+++ loop_user_create_no_email.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Creating this version on 10/01/2012 to deal with users who don't have
-# an email adress (Atlantis and Waikiki Offices)
-
 # in the example file just list their email addresses (fake) on line at a time
 # in this case it will be username@india.email.com
 
@@ -13,13 +11,13 @@ for email in `cat /Users/paul/shell_scripts/drush_user_create/atlantis/users_exa
       	sleep 1
       	echo ""
       	username=`echo $email | cut -d@ -f1`
-	
-	
 	echo "their username is $username"
 	echo " "
 	echo " "
 
 drush @live ucrt $username --mail=$email
+
+
 drush @live upwd $username --password=Hello123
 drush @live urol "General Role" $username
 drush @live urol "Atlantis" $username
@@ -28,10 +26,9 @@ echo " "
 echo "SQL Time"
 echo " "
 drush @live sqlq "UPDATE users SET DATA = (select data2 from dummytable) WHERE name = '$username'"
-echo $username >> "user_list_$(date +%Y_%m_%d).txt" # verify this works, mv to folder
+echo $username >> "archive_users_created/user_list_$(date +%Y_%m_%d).txt" # verify this works, mv to folder
 
 echo " "
 echo " "
 echo " users created"
-					
 done
