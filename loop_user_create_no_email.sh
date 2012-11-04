998361e92876b415751ac1e41a8e142221de9772
Paul <paulbosox@yahoo.com>
2012-11-03 22:01:30 -0400

    removed whitespace and added some spaces after account creation essage for clarity

--- loop_user_create_no_email.sh
+++ loop_user_create_no_email.sh
@@ -17,7 +17,7 @@ for email in `cat /Users/paul/shell_scripts/drush_user_create/atlantis/users_exa
 
 drush @live ucrt $username --mail=$email
 
- #trying to generate a random password
+# Generates a random password
 NUMBER=$[ ( $RANDOM % 900 )  + 100 ]
 PASSWORD="Hello$NUMBER"
 
@@ -29,9 +29,13 @@ echo " "
 echo "SQL Time"
 echo " "
 drush @live sqlq "UPDATE users SET DATA = (select data2 from dummytable) WHERE name = '$username'"
-echo "USERNAME:  $username  PASSWORD:  $PASSWORD" >> "archive_users_created/user_list_$(date +%Y_%m_%d).txt" # verify this works, mv to folder
+echo "USERNAME:  $username  PASSWORD:  $PASSWORD" >> "archive_users_created/user_list_$(date +%Y_%m_%d).txt" 
 
 echo " "
 echo " "
 echo " the user account $username was created"
+echo " "
+echo "******************************************** "
+echo " "
+echo " "
 done
